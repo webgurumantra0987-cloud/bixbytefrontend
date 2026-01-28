b713007522ada0aa891a66c4f7ebe052d6309004
webgurumantra0987-cloud <webgurumantra0987@gmail.com>
2026-01-28 12:31:43 +0530

    Cleanup: Removed accidental backend files from frontend repository

--- routes/contactRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-const { createContact, getAllContacts } = require("../controller/contactcontroller");
-const router = express.Router();
-
-
-router.post("/contact", createContact);
-router.get("/contacts", getAllContacts);
-
-module.exports = router;
